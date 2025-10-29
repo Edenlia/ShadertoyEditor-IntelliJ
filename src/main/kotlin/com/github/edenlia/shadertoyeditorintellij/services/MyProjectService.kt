10fd5c9bf390b9e6b4dbd3675b598ccd98feb99c
GitHub Action <action@github.com>
2025-10-29 04:06:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/edenlia/shadertoyeditorintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/edenlia/shadertoyeditorintellij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.edenlia.shadertoyeditorintellij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.edenlia.shadertoyeditorintellij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
